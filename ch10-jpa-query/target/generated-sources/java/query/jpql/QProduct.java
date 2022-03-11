69bf591662e8fd9d8c1f6050c0544cb3e1d31e10
hyukhub <cjh960504@gmail.com>
2022-03-11 17:05:59 +0900

    2022-03-11 queryDSL

---
+++ ch10-jpa-query/target/generated-sources/java/query/jpql/QProduct.java
@@ -0,0 +1,43 @@
+package query.jpql;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = -238322392L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final NumberPath<Integer> stockAmount = createNumber("stockAmount", Integer.class);
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
